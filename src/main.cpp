b6dd68b3669ef279cf139fe418a4c228941244fd
avandebrook <vandebra@my.erau.edu>
2019-10-19 22:41:46 -0400

    Added initial test code, libraries, and headers.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "PCANBasic.h"
+
+using namespace std;
+
+int main(void)
+{
+    TPCANStatus retval;
+    char message[256];
+
+    retval = CAN_Initialize(PCAN_USBBUS1, PCAN_BAUD_500K);
+
+    if (retval == PCAN_ERROR_OK) {
+        cout << "PCAN USB Bus Initialized" << endl;
+    } else {
+        CAN_GetErrorText(retval, 0, message);
+        cout << message << endl;
+    }
+
+    CAN_Uninitialize(PCAN_USBBUS1);
+    return 0;
+}
